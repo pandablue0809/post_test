39ea4cc6530542af53a92b6ee13a1e84f647b867
pandablue <bluep0415@gmail.com>
2024-08-01 00:55:48 +0900

    fix editer toolbar

--- my-app/src/pages/post/components/CreatePost.jsx
+++ my-app/src/pages/post/components/CreatePost.jsx
@@ -23,28 +23,32 @@ export default function CreatePost() {
                 editorState={editorState}
                 onEditorStateChange={handleEditorChange}
                 toolbar={{
-                    options: ['inline', 'list', 'textAlign', 'link','image'],
+                    inline: { inDropdown: false },
+                    list: { inDropdown: false },
+                    textAlign: { inDropdown: false },
+                    link: { inDropdown: false },
+                    history: { inDropdown: false },
                     image: {
-                        previewImage: true,
-                        uploadCallback: (file) => {
-                          return new Promise((resolve, reject) => {
-                            const reader = new FileReader();
-                            reader.onloadend = () => {
-                              resolve({
-                                data: {
-                                  url: reader.result,
-                                },
-                              });
-                            };
-            
-                            reader.onerror = (reason) => reject(reason);
-            
-                            reader.readAsDataURL(file);
-                          });
-                        },
-                        alt: { present: true, mandatory: true },
+                      previewImage: true,
+                      uploadCallback: (file) => {
+                        return new Promise((resolve, reject) => {
+                          const reader = new FileReader();
+                          reader.onloadend = () => {
+                            resolve({
+                              data: {
+                                url: reader.result,
+                              },
+                            });
+                          };
+        
+                          reader.onerror = (reason) => reject(reason);
+        
+                          reader.readAsDataURL(file);
+                        });
                       },
-                }}
+                      alt: { present: true, mandatory: true },
+                    },
+                  }}
             />
             <button className='flex felx-row justify-center gap-2 items-center w-[50%] py-3.5 mt-6 rounded-md bg-[radial-gradient(84.9%_100%_at_50%_0%,#8F00FF_0%,#532BC5_100%)] text-white'>
                 <FaCheck className='w-[24px] h-[24px]'/><span className='text-base font-[600]'>Post</span>
